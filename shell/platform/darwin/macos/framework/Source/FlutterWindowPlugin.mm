dba9e7e2c2633d5f0fb3f4e4bbf72d88fae7244c
Michael Goderbauer <goderbauer@google.com>
2024-01-25 15:37:48 -0800

    ++

--- shell/platform/darwin/macos/framework/Source/FlutterWindowPlugin.mm
+++ shell/platform/darwin/macos/framework/Source/FlutterWindowPlugin.mm
@@ -5,6 +5,14 @@
 #import "FlutterWindowPlugin.h"
 #import "FlutterEngine_Internal.h"
 
+@interface MyWindow : NSWindow
+@end
+@implementation MyWindow
+- (BOOL)canBecomeKeyWindow {
+  return YES;
+}
+@end
+
 static NSString* const kChannelName = @"flutter/window";
 
 @interface FlutterWindowPlugin ()
@@ -33,13 +41,28 @@ static NSString* const kChannelName = @"flutter/window";
   NSLog(@">>> method %@", call.method);
   if ([call.method isEqualToString:@"create"]) {
     NSLog(@">>> CREATE");
-    NSRect graphicsRect = NSMakeRect(400.0, 350.0, 860, 25);
-    NSWindow* window = [[NSWindow alloc]
+    NSDictionary* windowSpec = call.arguments;
+    NSLog(@">>> windowSpec %@", windowSpec);
+
+    NSWindow* parentWindow = [[NSApplication sharedApplication] mainWindow];
+    NSRect parentContentRect = [parentWindow contentRectForFrameRect:parentWindow.frame];
+    NSLog(@">>> parentContentRect %@", NSStringFromRect(parentContentRect));
+    double height = [windowSpec[@"height"] doubleValue];
+    double width = [windowSpec[@"width"] doubleValue];
+    double dy = NSMaxY(parentContentRect) - [windowSpec[@"offsetY"] doubleValue] - height;
+    double dx = NSMinX(parentContentRect) + [windowSpec[@"offsetX"] doubleValue];
+    NSRect graphicsRect = NSMakeRect(dx, dy, width, height);
+    NSLog(@">>> rect %@", NSStringFromRect(graphicsRect));
+
+    MyWindow* window = [[MyWindow alloc]
         initWithContentRect:graphicsRect
-                  styleMask:NSWindowStyleMaskBorderless  // NSWindowStyleMaskTitled |
-                                                         // NSWindowStyleMaskClosable |
-                                                         // NSWindowStyleMaskMiniaturizable |
-                                                         // NSWindowStyleMaskResizable
+                  styleMask:NSWindowStyleMaskBorderless
+                    //                         NSWindowStyleMaskTitled |
+                    //                                                          NSWindowStyleMaskClosable
+                    //                                                          |
+                    //                                                          NSWindowStyleMaskMiniaturizable
+                    //                                                          |
+                    //                                                          NSWindowStyleMaskResizable
                     backing:NSBackingStoreBuffered
                       defer:NO];
     NSRect windowFrame = [window frame];
@@ -47,11 +70,13 @@ static NSString* const kChannelName = @"flutter/window";
                                                                                   nibName:nil
                                                                                    bundle:nil];
     [window setContentViewController:viewController];
-    [window setReleasedWhenClosed:NO];  // Do not close entire app when window is closed.
     [window setFrame:windowFrame display:YES];
-    [window orderFront:nil];
-    [window setTitle:@"Hello"];
-    [window setIgnoresMouseEvents:YES];
+    //    [window orderFront:nil];
+    [window makeKeyAndOrderFront:self];
+    [window setReleasedWhenClosed:NO];  // Do not close entire app when window is closed.
+                                        //    [window setTitle:@"Hello"];
+                                        //    [window setIgnoresMouseEvents:YES];
+    [parentWindow addChildWindow:window ordered:NSWindowAbove];
 
     _windows[@(viewController.viewId)] = window;
     result(@(viewController.viewId));
